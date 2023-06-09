d6641fe1c9715fee777536869d30269018d21ac8
Joaovitorlemos <joaovitorlemos2015@gmail.com>
2023-06-09 18:06:25 -0300

    adding node server

--- js/index.js
+++ js/index.js
@@ -9,10 +9,9 @@ const routes = {
 function route(event) {
    event = event || window.event
    event.preventDefault()
-
-   // Pegar no histórico do window a url para meu pathname 
+   
    window.history.pushState({}, "", event.target.href)
-
+   
    handle()
 }
 
@@ -20,9 +19,6 @@ function handle() {
    const { pathname } = window.location
    const route = routes[pathname] || routes[404]
    
-   // 1. fetch() = vai buscar está rota | then() = quando concluir, execute essa função
-   // 2. text() = retorna os dados em forma de texto
-   // O primeiro then() pega os dados e transforma em texto, e no segundo then() pega o texto e transforma em HTML
    fetch(route)
    .then((data) => data.text())
    .then((html) => {
@@ -31,14 +27,9 @@ function handle() {
 }
 
 
-// Para já iniciar o projeto na página Home
 handle()
 
-// Para poder navegar pelas rotas nas setinhas do navegador
+
 window.onpopstate = () => handle()
-
-
-// Adicionando na window o route como uma função para poder disparar a função do route().
-// Porque quando coloquei os dados dentro do index.js ele não está conseguindo ler o evento de click(onclick) que eu coloquei no HTML para retornar minha função de callback
-// Posso colocar o route() como uma função que vai disparar a função do route()
 window.route = () => route()
+
